7c3a072dd648857a5267a5b5c123b6fa0682ca7c
Mohammed AlAzri <m.alazri@tayyars.com>
2023-02-22 20:49:46 +0400

    Doxyfile and basic readme added

---
+++ doc/html/index.js
@@ -0,0 +1,4 @@
+var index =
+[
+    [ "Welcome to Doxygen Demo", "index.html#autotoc_md0", null ]
+];
---
+++ doc/html/navtreeindex0.js
@@ -0,0 +1,7 @@
+var NAVTREEINDEX0 =
+{
+"index.html":[],
+"md_readme.html":[0],
+"md_readme.html#autotoc_md1":[0,0],
+"pages.html":[]
+};
